c7b65672eeddbc9c5ec0552c3f7d5e33ba2697ca
belegoth <belegoth@mail.ru>
2016-11-14 18:34:24 +0300

    Test new branch

--- httpchecks/httpcheck.py
+++ httpchecks/httpcheck.py
@@ -7,27 +7,14 @@ except ImportError:
 
 import yaml
 import socket
-import time
 import logging
 import argparse
-from bs4 import BeautifulSoup
-import re
 import sys
-import json
-import requests
+import os
+import ssl
+import datetime
 
 log = logging.getLogger(__name__)
-
-# def send_metric_to_carbon(metric_name, value, graphite_host, graphite_port, ts=None):
-#     if not ts:
-#         ts = int(time.time())
-#     message = '%s %s %d\n' % (metric_name, value, int(ts))
-#     log.info("sending to graphite %s", message)
-#     sock = socket.socket()
-#     sock.connect((graphite_host, graphite_port))
-#     sock.sendall(message)
-#     sock.close()
-
 # Monkey-patch.
 gmonkey.patch_all(thread=False, select=False)
 
@@ -53,6 +40,8 @@ class AsyncRequest(object):
         self.session = kwargs.pop('session', None)
         if self.session is None:
             self.session = Session()
+        if self.session is None:
+            self.session = Session()
 
         callback = kwargs.pop('callback', None)
         if callback:
@@ -65,7 +54,8 @@ class AsyncRequest(object):
         self.error = None
         self.name = None
         self.waiting_status_code = None
-        self.check_text = self.check_html = None
+        self.check_cert = None
+
 
     def send(self, **kwargs):
         """
@@ -83,12 +73,55 @@ class AsyncRequest(object):
         except Exception as e:
             self.response = None
             self.error = e
-            log.exception("[%s] gave exception" % self.url)
+            # log.exception("[%s] gave exception" % self.url)
             return
         return self.response
 
     def __repr__(self):
         return "<AsyncRequest %s>" % self.url
+
+def ssl_expiry_datetime(hostname):
+    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+
+    context = ssl.create_default_context()
+    conn = context.wrap_socket(
+        socket.socket(socket.AF_INET),
+        server_hostname=hostname,
+    )
+    # 3 second timeout because Lambda has runtime limitations
+    conn.settimeout(3.0)
+
+    conn.connect((hostname, 443))
+    ssl_info = conn.getpeercert()
+    # parse the string from the certificate into a Python datetime object
+    return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+
+def ssl_valid_time_remaining(hostname):
+    """Get the number of days left in a cert's lifetime."""
+    expires = ssl_expiry_datetime(hostname)
+    log.debug(
+        "SSL cert for %s expires at %s",
+        hostname, expires.isoformat()
+    )
+    return expires - datetime.datetime.utcnow()
+
+def ssl_expires_in(hostname, buffer_days=14):
+    """Check if `hostname` SSL cert expires is within `buffer_days`.
+
+    Raises `AlreadyExpired` if the cert is past due
+    """
+    remaining = ssl_valid_time_remaining(hostname)
+
+    # if the cert expires in less than two weeks, we should reissue it
+    if remaining < datetime.timedelta(days=0):
+        # cert has already expired - uhoh!
+        log.warn("Cert expired %s days ago" % remaining.days)
+    elif remaining < datetime.timedelta(days=buffer_days):
+        # expires sooner than the buffer
+        return True
+    else:
+        # everything is fine
+        return False
 
 def send(r, pool=None, stream=False, callback=None):
     """Sends the request object using the specified pool. If a pool isn't
@@ -117,37 +150,6 @@ def map_requests(requests, stream=False, size=None):
     return [r for r in requests]
 
 
-def check_html(req):
-    if not req.check_html:
-        return True
-
-    soup = BeautifulSoup(req.response.content)
-    for html_check in req.check_html:
-        for selector, v in html_check.iteritems():
-            elements = soup.select(selector)
-            if not elements:
-                log.debug('[%s] couldn\'t find any elements matching %s', req.url, selector)
-                return False
-            for el in elements:
-                log.debug('[%s] checking element %s', req.url, el)
-                if v.startswith('~'):
-                    reg_exp = v.split('~/')[1][:-1]
-                    log.debug('[%s] checking with reqexp ~/%s/', req.url, reg_exp)
-                    if not re.match(reg_exp, el.get_text()):
-                        log.debug('[%s] checking with reqexp %s failed', req.url, reg_exp)
-                        return False
-                    log.debug('[%s] checking with reqexp %s passed', req.url, reg_exp)
-                else:
-                    if not el.get_text() == v:
-                        log.debug('[%s] failed because el text doesnt match %s', req.url, v)
-                        return False
-    return True
-
-def check_text(req):
-    if not req.check_text:
-        return True
-    log.debug("[%s] response %s ", req.url, req.response.content)
-    return req.check_text in req.response.content
 
 def check_status_code(req):
     log.debug("[%s] checking status code waiting: %s actual: %s", req.url, req.waiting_status_code, req.response.status_code)
@@ -160,20 +162,6 @@ def check_response(req):
     log.debug("[%s] response %s ", req.url, resp_content)
     return req.response
 
-def check_json(req):
-    from jsonpath_rw import jsonpath, parse
-
-    if not req.check_json:
-        return True
-
-    j = json.loads(req.response.content)
-    for check in req.check_json:
-        for k, v in check.items():
-            path = parse(k)
-            matches = path.find(j)
-            if not matches:
-                return False
-            return matches[0].value == v
 
 
 def notify_by_ovo(url, channel, username, description, icon_emoji):
@@ -182,6 +170,7 @@ def notify_by_ovo(url, channel, username, description, icon_emoji):
 
 def get_request(k, urlconf, callback=None, session=None):
     r = AsyncRequest(
+
             method = urlconf.get('method', 'GET'),
             timeout = urlconf.get('timeout', 5.0),
             url = urlconf['url'],
@@ -196,18 +185,12 @@ def get_request(k, urlconf, callback=None, session=None):
     if not r.waiting_status_code:
         r.waiting_status_code = [200]
 
-    r.check_text = urlconf.get('text', None)
-    r.check_html = urlconf.get('html', None)
-    r.check_json = urlconf.get('json', None)
     return r
 
 checks = [
     check_response,
-    check_status_code,
-    check_text,
-    check_html,
-    check_json
-]
+    check_status_code
+ ]
 
 
 ready = gevent.event.Event()
@@ -219,10 +202,6 @@ sync_map = []
 def finished(result):
     global exit_code, finished_jobs
     finished_jobs += 1
-
-    # if not result:
-    #     # if any of the tests fail we fail too
-    #     exit_code = 2
 
     if finished_jobs == len(sync_map):
         log.info('all waiting jobs are completed.')
@@ -242,7 +221,6 @@ class SessionedChecks(object):
     def add(self, rs):
         self.steps.append(rs)
 
-        # trte
 
     def next(self):
         try:
@@ -289,31 +267,20 @@ def main():
 
     logging.basicConfig(level=config['settings'].get('log_level', 'DEBUG').upper())
 
-    # graphite_host = config['settings'].get('graphite_host')
-    # try:
-    #     graphite_port = int(config['settings'].get('graphite_port'))
-    # except:
-    #     graphite_port = None
-
     rs = []
 
     for k, urlconf in config['urls'].items():
+
         # different sessions can run in parallel
         if isinstance(urlconf, list):
-            sc = SessionedChecks(name=k)
             for c in urlconf:
-                # but individual urls in session must run in sync.
-                r = get_request(k, c, session=sc.session)
-                sc.add(r)
-            sync_map.append(sc)
+                r = get_request(k,c)
+                rs.append(r)
 
         else:
-            # these can run in parallel, because they dont need to have a defined flow
             r = get_request(k, urlconf)
             rs.append(r)
 
-    for sm in sync_map:
-        sm.run()
 
     reqs = map_requests(rs, size=config.get('pool_size', 10))
 
@@ -321,36 +288,33 @@ def main():
         elapsed = -1
         failed = False
         for check in checks:
+
             if not check(req):
                 failed = True
                 log.critical('[%s] FAILED check - %s', req.name, check.__name__)
-                ovo_config = config['settings'].get('ovo', None)
-                if ovo_config:
-                    notify_by_ovo(
-                        # url = slack_config['url'],
-                        # channel  = slack_config['channel'],
-                        # username  = slack_config['username'],
-                        # description  = '[%s] FAILED check - %s - %s' % (req.name, req.url, check.__name__),
-                        # icon_emoji = slack_config['icon_emoji']
-                    )
+
+                # ovo_config = config['settings'].get('ovo', None)
+                # if ovo_config:
+                #     notify_by_ovo(
+                #         print("Send to OVO %s", req.name)
+                #         # url = slack_config['url'],
+                #         # channel  = slack_config['channel'],
+                #         # username  = slack_config['username'],
+                #         # description  = '[%s] FAILED check - %s - %s' % (req.name, req.url, check.__name__),
+                #         # icon_emoji = slack_config['icon_emoji']
+                #     )
+
                 break
+            else:
+                log.info(req.name +" " + check.__name__ + " OK")
 
         if not failed:
             elapsed = req.response.elapsed.total_seconds()
+
         else:
             exit_code = 2
+        log.info("[%s] completed in %s", req.name, elapsed)
 
-        # if not config['settings'].get('dry_run', False) \
-        #     and graphite_host and graphite_port:
-        #     send_metric_to_carbon('http_check.%s' % req.name,
-        #                       elapsed,
-        #                       graphite_host=graphite_host,
-        #                       graphite_port=graphite_port)
-        # else:
-            log.info("[%s] completed in %s", req.name, elapsed)
-
-    if sync_map:
-        ready.wait()
 
     sys.exit(exit_code)
 
